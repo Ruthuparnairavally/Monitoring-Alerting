809b7dac12b797bfc98171f7e9962d1a0ce72e44
Ruthuparna I R <88615324+Ruthuparnairavally@users.noreply.github.com>
2025-12-18 14:58:13 +0530

    Update grafna-promethous.sh
    int id

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -137,7 +137,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "4bf6bcd8cb9b4607d047c4b6c2d333d6" #add key pagerduty 
+      - routing_key: "040b995e2e494803c0d45b6343c39da2" #add key pagerduty 
         severity: "critical"
 EOF
 
